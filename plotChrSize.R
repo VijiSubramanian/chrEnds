699e65501396e1568460ac8d8a886bfa095ece91
VijiSubramanian <32377396+VijiSubramanian@users.noreply.github.com>
2019-01-17 14:55:37 -0500

    Update plotChrSize.R

--- plotChrSize.R
+++ plotChrSize.R
@@ -1,11 +1,11 @@
 
 ##################################################################################################
 ####                                                                                         #####
-####         		function: Chromosome size plot from ChIP-Seq data                		 #####
+####         		function: Chromosome size plot from ChIP-Seq data                    #####
 ####                                                                                         #####
 ####                        Viji Subramanian: July 7, 2017                                   #####
-#### 																						 #####
-####                          																 #####
+#### 											     #####										 #####
+####                          								     #####							 #####
 ####                                                                                         #####
 ##################################################################################################
 
@@ -45,22 +45,26 @@ plotChrSize = function(Sample, genome = 'SK1Yue') {
 	message(paste0('\n\nCompleted in ', round((proc.time()[3] - ptm[3]), 2), ' sec.\n'))
 	return(df)
 	}
-	
-# par(las=1)
-# plot(log2(AH574Hop1T2_avgChr[,3]), log2(AH574Hop1T2_avgChr[,2]), type="p", ylim=c(-0.4,1.3), xlim=c(17.7, 20.7), pch = 16, cex = 3, col="ivory3", xlab="Chromosome Size (lKb) log2 scale", ylab="Average Hop1 binding (log2 scale)", bty='n')
-# points(log2(AH574Hop1T3_avgChr[,3]), log2(AH574Hop1T3_avgChr[,2]), type="p", pch = 16, cex = 3, col="ivory4")
-# points(log2(AH574Hop1T4_avgChr[,3]), log2(AH574Hop1T4_avgChr[,2]), type="p", pch = 16, cex = 3, col="grey30")
-#plot(log2(AH6179T3Hop1rep_avgChr[,3]), log2(AH6179T3Hop1rep_avgChr[,2]), type="p", ylim=c(-0.3,1.2), xlim=c(17.7, 20.7), pch = 16, cex = 3, col="lawngreen", xlab="Chromosome Size (Kb) log2 scale", ylab="Average binding log2 scale", bty='n')
-#points(log2(AH6179T6Hop1rep_avgChr[,3]), log2(AH6179T6Hop1rep_avgChr[,2]), type="p", pch = 16, cex = 3, col="forestgreen")
-#abline(lm(log2(AH6179T3Hop1rep_avgChr[,2])~log2(AH6179T3Hop1rep_avgChr[,3])), col = "lawngreen", lwd=3, lty=3)
-#abline(lm(log2(AH6179T6Hop1rep_avgChr[,2])~log2(AH6179T6Hop1rep_avgChr[,3])), col = "forestgreen", lwd=3, lty=3)
 
-#plot(log2(AH6639T3Hop1rep_avgChr[,3]), log2(AH6639T3Hop1rep_avgChr[,2]), type="p", ylim=c(-0.3,0.8), xlim=c(17.7, 20.7), pch = 16, cex = 3, col="darkolivegreen1", xlab="Chromosome Size (Kb) log2 scale", ylab="Average binding log2 scale", bty='n')
-#points(log2(AH6639T6Hop1rep_avgChr[,3]), log2(AH6639T6Hop1rep_avgChr[,2]), type="p", pch = 16, cex = 3, col="darkolivegreen4")
-#abline(lm(log2(AH6639T3Hop1rep_avgChr[,2])~log2(AH6639T3Hop1rep_avgChr[,3])), col = "darkolivegreen1", lwd=3, lty=3)
-#abline(lm(log2(AH6639T6Hop1rep_avgChr[,2])~log2(AH6639T6Hop1rep_avgChr[,3])), col = "darkolivegreen4", lwd=3, lty=3)
 
-#AH6179T3T6Hop1rep_avgChrAnova <- anova_seq_signal(AH6179T3Hop1rep,AH6179T6Hop1rep)
-##log2(chrSize):timepointlate -0.29832    0.05441  -5.483 7.42e-06 ***
-#AH6639Hop1anova <- anova_seq_signal(AH6639T3Hop1rep,AH6639T6Hop1rep)
-##log2(chrSize):timepointlate   0.2041     0.0512   3.987 0.000435 ***
+####### 
+####### source above script
+#source("/Volumes/LabShare/Viji/Scripts/ChIPSeq_R/SK1Yue_Bed/plotChrSize.R")
+
+###### use bedgraph data as input for source script 
+#AH6179Hop1T3chrSize <- plotChrSize(AH6179Hop1T3)
+#AH6179Hop1T6chrSize <- plotChrSize(AH6179Hop1T6)
+
+###### plot chromosome size
+#plot(log2(AH6179Hop1T3chrSize[,3]), log2(AH6179Hop1T3chrSize[,2]), type="p", ylim=c(-0.4,1.2), xlim=c(17.7, 20.7), pch = 15, cex = 3, col="darkseagreen", xlab="Chromosome Size (Kb) log2 scale", ylab="Average Hop1 binding (log2 scale)", bty='n')
+#points(log2(AH6179Hop1T6chrSize[,3]), log2(AH6179Hop1T6chrSize[,2]), type="p", pch = 16, cex = 3, col="forestgreen")
+#abline(lm(log2(AH6179Hop1T3chrSize[,2])~ log2(AH6179Hop1T3chrSize[,3])), col="darkseagreen", lwd=3, lty=3)
+#abline(lm(log2(AH6179Hop1T6chrSize[,2])~ log2(AH6179Hop1T6chrSize[,3])), col="forestgreen", lwd=3, lty=3)
+#legend(19.5,1.19, c("T = 3hr ndt80", "T = 6hr ndt80"), text.col = c( "darkseagreen", "forestgreen"), bty = "n")
+
+###### calculate fit of the regression line
+#AH6179Hop1T3chrSizeSlope <- capture.output(summary(lm(log2(avrg_signal)~log2(chrSize), AH6179Hop1T3chrSize)))
+#cat(AH6179Hop1T3chrSizeSlope, file= "AH6179Hop1T3chrSizeSlope", sep = '\n', append = TRUE)
+#AH6179Hop1T6chrSizeSlope <- capture.output(summary(lm(log2(avrg_signal)~log2(chrSize), AH6179Hop1T6chrSize)))
+#cat(AH6179Hop1T6chrSizeSlope, file= "AH6179Hop1T6chrSizeSlope", sep = '\n', append = TRUE)
+
